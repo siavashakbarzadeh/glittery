6f6b8add94e3c9dddcb1559e41942221e6bc21dc
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-29 12:40:37 +0200

    a

---
+++ platform/themes/martfury/partials/breadcrumbs.blade.php
@@ -0,0 +1,18 @@
+<ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+    @foreach ($crumbs = Theme::breadcrumb()->getCrumbs() as $i => $crumb)
+        @if ($i != (count($crumbs) - 1))
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                <a href="{{ $crumb['url'] }}" itemprop="item">{!! BaseHelper::clean($crumb['label']) !!}</a>
+                <span class="extra-breadcrumb-name"></span>
+                <meta itemprop="name" content="{{ $crumb['label'] }}" />
+                <meta itemprop="position" content="{{ $i + 1}}" />
+            </li>
+        @else
+            <li aria-current="page" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                <span>{!! BaseHelper::clean($crumb['label']) !!}</span>
+                <meta itemprop="name" content="{{ $crumb['label'] }}" />
+                <meta itemprop="position" content="{{ $i + 1}}" />
+            </li>
+        @endif
+    @endforeach
+</ul>
